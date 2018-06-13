804e4c60280990a14832efa6f18cbb0ba354893f
michel zarpelon <michelzarpe@gmail.com>
2018-06-13 17:01:17 -0300

    Criado projeto CursoMcMz

---
+++ 2 - Implementação do modelo conceitual/criacao do projeto Spring Boot/cursomcmz/src/main/java/com/michelzarpelon/cursomcmz/CursomcmzApplication.java
@@ -0,0 +1,14 @@
+package com.michelzarpelon.cursomcmz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcmzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcmzApplication.class, args);
+	}
+}
+
+/*cria tudo sozinho*/
