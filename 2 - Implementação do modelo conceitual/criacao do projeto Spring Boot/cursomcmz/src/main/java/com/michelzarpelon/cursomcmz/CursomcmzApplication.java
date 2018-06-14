c64bff6c7f388d4ad6461bbb6f86de6d07b0b5dc
michel zarpelon <michelzarpe@gmail.com>
2018-06-14 17:17:44 -0300

    CursoMcMz Categoria e Produto com Associacao + ProdutoRepository

--- 2 - Implementação do modelo conceitual/criacao do projeto Spring Boot/cursomcmz/src/main/java/com/michelzarpelon/cursomcmz/CursomcmzApplication.java
+++ 2 - Implementação do modelo conceitual/criacao do projeto Spring Boot/cursomcmz/src/main/java/com/michelzarpelon/cursomcmz/CursomcmzApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.michelzarpelon.cursomcmz.domain.Categoria;
+import com.michelzarpelon.cursomcmz.domain.Produto;
 import com.michelzarpelon.cursomcmz.repositories.CategoriaRepository;
+import com.michelzarpelon.cursomcmz.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcmzApplication implements CommandLineRunner{
@@ -16,7 +18,8 @@ public class CursomcmzApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository repositorioCategoria;
-	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcmzApplication.class, args);
@@ -26,8 +29,21 @@ public class CursomcmzApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p1));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		repositorioCategoria.save(Arrays.asList(cat1,cat2));
+		produtoRepository.save(Arrays.asList(p1,p2,p3));
+		
 	}
 }
 
-/*cria tudo sozinho*/
---
+++ 2 - Implementação do modelo conceitual/criacao do projeto Spring Boot/cursomcmz/src/main/java/com/michelzarpelon/cursomcmz/services/ProdutoService.java
@@ -0,0 +1,21 @@
+package com.michelzarpelon.cursomcmz.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.michelzarpelon.cursomcmz.domain.Produto;
+import com.michelzarpelon.cursomcmz.repositories.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	private Produto buscar(Integer id) {
+		Produto produto = produtoRepository.findOne(id);
+		return produto;
+	}
+	
+	
+}
