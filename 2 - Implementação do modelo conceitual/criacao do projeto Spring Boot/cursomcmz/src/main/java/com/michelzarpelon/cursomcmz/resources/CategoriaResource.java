b0a165263577e09f454b156d386ec6506c703bcf
michel zarpelon <michelzarpe@gmail.com>
2018-06-14 08:58:52 -0300

    CursoMcMZ - Testando primeira Classe de dominio

--- 2 - Implementação do modelo conceitual/criacao do projeto Spring Boot/cursomcmz/src/main/java/com/michelzarpelon/cursomcmz/resources/CategoriaResource.java
+++ 2 - Implementação do modelo conceitual/criacao do projeto Spring Boot/cursomcmz/src/main/java/com/michelzarpelon/cursomcmz/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package com.michelzarpelon.cursomcmz.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.michelzarpelon.cursomcmz.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		List<Categoria> listCategoria = new ArrayList<>();
+		listCategoria.add(cat2);
+		listCategoria.add(cat1);
+		return listCategoria;
 	}
 
 }
