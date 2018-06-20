cfbc40d8e59a37440e90521d019a29fea2393a4a
michel zarpelon <michelzarpe@gmail.com>
2018-06-20 15:20:30 -0300

    CursoMcMz - Criado Metodo POST para inserir Categoria

--- 2 - Implementação do modelo conceitual/criacao do projeto Spring Boot/cursomcmz/src/main/java/com/michelzarpelon/cursomcmz/services/CategoriaService.java
+++ 2 - Implementação do modelo conceitual/criacao do projeto Spring Boot/cursomcmz/src/main/java/com/michelzarpelon/cursomcmz/services/CategoriaService.java
@@ -8,17 +8,22 @@ import com.michelzarpelon.cursomcmz.services.execeptions.ObjectNotFoundException
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repositorioCategoria;
-	
+
 	public Categoria buscar(Integer id) {
 		Categoria categoria = repositorioCategoria.findOne(id);
-		if(categoria==null) {
-			throw new ObjectNotFoundException("Objeto não encontrado: "+id+", Tipo do objeto: "+Categoria.class.getName());
+		if (categoria == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado: " + id + ", Tipo do objeto: " + Categoria.class.getName());
 		}
 		return categoria;
 	}
-	
-	
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repositorioCategoria.save(obj);
+	}
+
 }
