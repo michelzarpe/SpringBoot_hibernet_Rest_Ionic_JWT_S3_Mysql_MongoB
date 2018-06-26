93ed3736dee79cac89b273c77d99d98d40098969
michel zarpelon <michelzarpe@gmail.com>
2018-06-26 15:55:24 -0300

    CursoMcMz - Criado EndPoint Produtos, buscando produtos por nome e categorias

--- 2 - Implementação do modelo conceitual/criacao do projeto Spring Boot/cursomcmz/src/main/java/com/michelzarpelon/cursomcmz/repositories/CategoriaRepository.java
+++ 2 - Implementação do modelo conceitual/criacao do projeto Spring Boot/cursomcmz/src/main/java/com/michelzarpelon/cursomcmz/repositories/CategoriaRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.michelzarpelon.cursomcmz.domain.Categoria;
 
+
+/*
+https://docs.spring.io/spring-data/jpa/docs/current/reference/html/
+*/
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {/*integer é o tipo do atributo identificador*/
 
